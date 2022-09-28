cf0083c5c964037e93848b5993e252fed4a9d74d
yuliaokta <114564209+yuliaokta@users.noreply.github.com>
2022-09-27 19:32:14 -1200

    Add files via upload
    modul 3 math operators

--- Program.cs
+++ Program.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 
-namespace literalvalues
+namespace mathoperators
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("true");
-            Console.WriteLine("false");
+            Console.WriteLine("Modulus of 200 / 5 : " + (200 % 5));
+Console.WriteLine("Modulus of 7 / 5 : " + (7 % 5));
         }
     }
 }

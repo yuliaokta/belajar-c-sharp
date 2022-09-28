1b3eb40a028aeeaed25e5aa0695df31391b9d1c6
yuliaokta <114564209+yuliaokta@users.noreply.github.com>
2022-09-27 19:30:43 -1200

    Add files via upload
    modul 2 literal values

--- Program.cs
+++ Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 
-namespace helloworld
+namespace literalvalues
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("true");
+            Console.WriteLine("false");
         }
     }
 }
